bd62fb63e7940550659947170546f507e668f759
Told-Ya <60781874+Told-Ya@users.noreply.github.com>
2020-07-22 21:45:46 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.querySelector('.i-11').onkeydown = () => {
+    document.querySelectorAll('.key').forEach((element) => {
+        element.classList.remove('key_active'); // Убирает подсветку клавиши TAB, тк при нажатии TAB фокус с инпута пропадает и подсветка у TAB не убирается
+    });
     document.querySelector('.key[data="'+event.code+'"]').classList.add('key_active');
 };
 document.querySelector('.i-11').onkeyup = () => {
